73e5f8275314ae66504c7d9449b6a60b88bd7a01
이재훈 <potential1205@naver.com>
2022-05-16 14:14:55 +0900

    version3

--- src/main/java/Group6/capstone/InitDb.java
+++ src/main/java/Group6/capstone/InitDb.java
@@ -28,15 +28,13 @@ public class InitDb {
     static class InitService {
         private final EntityManager em;
         public void dbInit1() {
-            Point point1 = Point.createPoint("어린이대공원 입구", 350, 84, 37.549409, 127.075770);
-            Point point2 = Point.createPoint("광진 광장", 210, 40, 37.547775, 127.073700);
-            Point point3 = Point.createPoint("세종초등학교 입구", 270, 35, 37.552667, 127.073059);
-            Point point4 = Point.createPoint("영실관 입구 ", 150, 42, 37.552101, 127.073609);
-            Point point5 = Point.createPoint("애지헌 앞 도로", 130, 84, 37.551001, 127.074609);
-            Point point6 = Point.createPoint("세종대학교 운동장", 10, 15, 37.550257, 127.075006);
-            Point point7 = Point.createPoint("세종대학교 입구", 140, 84, 37.548805, 127.074895);
+            Point point1 = Point.createPoint("광개토관", 150, 42, 37.550269, 127.073362);
+            Point point2 = Point.createPoint("영실관 입구", 150, 42, 37.552101, 127.073609);
+            Point point3 = Point.createPoint("애지헌", 130, 84, 37.551001, 127.074609);
+            Point point4 = Point.createPoint("AI센터", 10, 15, 37.550257, 127.075006);
+            Point point5 = Point.createPoint("정문", 140, 84, 37.548805, 127.074895);
 
-            List<Point> pointList = new ArrayList<>(Arrays.asList(point1,point2,point3,point4, point5, point6, point7));
+            List<Point> pointList = new ArrayList<>(Arrays.asList(point1,point2,point3,point4, point5));
             for (Point point : pointList) {
                 em.persist(point);
             }
